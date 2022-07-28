b4d52ab22b4bdb40c7bd54013ca6986742146907
zakaria-bn <72271949+zakaria-bn@users.noreply.github.com>
2022-07-28 02:35:56 +0100

    done

---
+++ JS-Bootcaamp 001-009/JS-Bp 001-009-03/03.js
@@ -0,0 +1,6 @@
+console.log(
+  "%cElzero  %cWeb %cSchool",
+  "color:red;  font-size:40px;",
+  "color:blue; font-weight:bold; font-size:40px;",
+  "color:green;  font-size:40px;"
+);
---
+++ JS-Bootcaamp 010-017/JS-Bp 010-017-04/04.js
@@ -0,0 +1,5 @@
+let a = 21;
+let b = 20;
+
+console.log("_" + a + "_" + b + a + "_" + b + a + "_" + b + a + "_" + b + "_");
+console.log(`_${a}_${b}${a}_${b}${a}_${b}${a}_${b}_`);
