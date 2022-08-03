a088bb9f70d3da971c0520bf6454ab34eeb82486
zakaria-bn <72271949+zakaria-bn@users.noreply.github.com>
2022-08-03 18:51:06 +0100

    done

--- JS-Bootcaamp 018-022-challenge-01-Plus And Minus Puzzle/challenge-02.js
+++ JS-Bootcaamp 018-022-challenge-01-Plus And Minus Puzzle/challenge-02.js
@@ -103,17 +103,3 @@ console.log(--c + +b + --a * +b++ - +b * a + --a - +true);
   - Explain: no increment
   */
 
-  /*
-  Challenge 2
-*/
-
-let d = "-100";
-let e = "20";
-let f = 30;
-let g = true;
-
-// Only Use Variables Value
-// Do Not Use Variable Twice
-
-console.log(-d * +e ); // 2000
-console.log(-d + ++e * ++g + ++f); // 173
---
+++ Js-Bootcamp 031-039/Js-Bootcamp 033-037/Js-Bootcamp 033-037-03/01.js
@@ -0,0 +1,17 @@
+//Variables
+let numberOne = 10;
+let numberTwo = 20;
+
+//Output
+console.log(numberOne + "" + numberTwo)
+console.log(typeof (numberOne + "" + numberTwo))
+console.log(`${numberOne}${numberTwo}`)
+console.log(typeof (`${numberOne}${numberTwo}`))
+
+
+//Normal concatenate
+console.log(numberTwo + "\n" + numberOne);
+
+//Template literals Way
+console.log(`${numberTwo}
+${numberOne}`);
