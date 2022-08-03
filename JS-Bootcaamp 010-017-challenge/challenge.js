892344a2e499dc072c44c85cfc38ba346893bbde
zakaria-bn <72271949+zakaria-bn@users.noreply.github.com>
2022-08-03 14:04:14 +0100

    done

---
+++ JS-Bootcaamp 010-017-challenge/challenge.js
@@ -0,0 +1,12 @@
+let the_Title = "Hello Elzero",
+  the_Description = "Elzero Web School",
+  the_Date = "25/10";
+
+let a = `
+  <div class = "card">
+    <h3>${the_Title}</h3>
+    <p>${the_Description}</p>
+    <span>${the_Date}</span>
+  </div>
+`;
+document.write(a.repeat(4));
