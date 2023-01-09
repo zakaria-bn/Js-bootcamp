e6bcf94a43a39fa90b36c0155d9739fd31c669b8
Zakaria Benbouzid <72271949+zakaria-bn@users.noreply.github.com>
2023-01-09 18:39:33 +0100

    done

--- JS-Bootcamp 048-056/Js-Bootcamp 048-053/JS-Bootcamp 048-053-05/05.js
+++ JS-Bootcamp 048-056/Js-Bootcamp 048-053/JS-Bootcamp 048-053-05/05.js
@@ -5,7 +5,7 @@ for (let i = letter.length - letter.length; i < friends.length; i++) {
   if (friends[i].startsWith(letter.toUpperCase())) {
     continue;
   }
-  console.log(`${Number} => ${friends[i]}`);
+  console.log(`"${Number} => ${friends[i]}"`);
   Number++;
 }
 
