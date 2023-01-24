1d4a2b273fc1ef19eb6baf31e7ed7df27e213361
Zakaria Benbouzid <72271949+zakaria-bn@users.noreply.github.com>
2023-01-24 20:47:23 +0100

    done

--- JS-Bootcamp 048-056/Js-Bootcamp 048-053/JS-Bootcamp 048-053-07/07.js
+++ JS-Bootcamp 048-056/Js-Bootcamp 048-053/JS-Bootcamp 048-053-07/07.js
@@ -1,8 +1,8 @@
 let start = 0;
 let mix = [1, 2, 3, "A", "B", "C", 4];
 
-for (let i = start + mix.indexOf(2); i < mix.length; i++) {
-  if (typeof mix[i] === "string") {
+for (let i = start; i < mix.length; i++) {
+  if (mix[i] === mix[start] || typeof mix[i] === "string") {
     continue;
   }
   console.log(mix[i]);
