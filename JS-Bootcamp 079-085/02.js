9c5271654482cbf752972dc4cb98377f36fa71e0
zakaria-bn <benbouzakoo@gmail.com>
2023-06-20 18:45:53 +0100

    done

---
+++ JS-Bootcamp 079-085/02.js
@@ -0,0 +1,24 @@
+
+// Method One
+// Create Your Object Here
+let objMethodOne = {
+  property:"Method One"
+};
+console.log(objMethodOne.property); // "Method One"
+
+// Method Two
+// Create Your Object Here
+let objMethodTwo =Object.assign({},{property:"Method Two",})
+objMethodTwo.property="Method Two";
+console.log(objMethodTwo.property); // "Method Two"
+
+// Method Three
+// Create Your Object Here
+let objMethodThree = new Object({property : "Method Three",});
+console.log(objMethodThree.property); // "Method Three"
+
+// Method Four
+// Create Your Object Here
+let objMethodFour = Object.create({});
+objMethodFour.property= "Method Four";
+console.log(objMethodFour.property); // "Method Four"
