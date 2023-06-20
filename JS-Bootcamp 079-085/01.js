ed02cea21d2b814edd5fcc308ed2377c980e51dc
zakaria-bn <benbouzakoo@gmail.com>
2023-06-20 16:17:36 +0100

    done

---
+++ JS-Bootcamp 079-085/01.js
@@ -0,0 +1,16 @@
+
+let member = {
+//Properties
+name: "Elzero",
+age: 38 ,
+country: "Egypt",
+fullDetails: function(){
+return `My Name Is ${member.name} , My Age Is ${menubar.age}, I Live in ${member.country}`;
+}
+}
+console.log(member.name);
+console.log(member.age);
+console.log(member.country);
+console.log(member.fullDetails());
+
+
