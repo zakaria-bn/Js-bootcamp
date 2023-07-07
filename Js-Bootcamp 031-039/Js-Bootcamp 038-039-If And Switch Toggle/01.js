2679e2623d3818c2837b3abf1e5127bc63d2be05
zakaria-bn <benbouzakoo@gmail.com>
2023-07-07 15:53:36 +0100

    done

--- Js-Bootcamp 031-039/Js-Bootcamp 038-039-If And Switch Toggle/01.js
+++ Js-Bootcamp 031-039/Js-Bootcamp 038-039-If And Switch Toggle/01.js
@@ -14,18 +14,18 @@ if (job === "Manager") {
   salary = 4000;
 }
 
-switch (1) {
-  case 0:
+switch (job) {
+  case "Manager":
     salary = 8000;
     console.log(`Manager salary is ${salary}`);
     break;
-  case 1:
-  case 2:
+  case "it":
+  case "Support":
     salary = 6000;
     console.log(`it & Support salary are ${salary}`);
     break;
-  case 3:
-  case 4:
+  case "Developer":
+  case "Designer":
     salary = 7000;
     console.log(`Developer & Designer are ${salary}`);
     break;
@@ -36,7 +36,7 @@ switch (1) {
 
 //solution 2
 
-let holidays = 0;
+let holidays = 5;
 let money = 0;
 
 switch (holidays) {
@@ -66,14 +66,18 @@ switch (holidays) {
     console.log(`My Money is ${money}`);
 }
 
-if (holidays === 0) {
+if (holidays == 0) {
   money = 5000;
-} else if (holidays === 1 || 2) {
+  console.log(`My Money is ${money}`);
+} else if (holidays == 1 || holidays == 2) {
   money = 3000;
-} else if (holidays === 3) {
+  console.log(`My Money is ${money}`);
+} else if (holidays == 3) {
   money = 2000;
-} else if (holidays === 4) {
+  console.log(`My Money is ${money}`);
+} else if (holidays == 4) {
   money = 1000;
+  console.log(`My Money is ${money}`);
 } else {
   money = 0;
 }
